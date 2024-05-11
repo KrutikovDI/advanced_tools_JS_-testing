b934fc5dfac9e1a53b8194790dc0125122246bd9
Krutikov Danil <121032966+KrutikovDI@users.noreply.github.com>
2024-05-11 15:16:14 +0700

    Update cardActive.test.js

--- src/js/__test__/cardActive.test.js
+++ src/js/__test__/cardActive.test.js
@@ -1,6 +1,13 @@
 import { cardActve } from "../main/cardActive";
 
 test("проверяем отсутствие класса 'mask' у элемента Discover", () => {
+    document.body.innerHTML = `<div class="cards-list">
+    <div class="card-conteiner">
+      <div class="card mask" id="Discover">
+        <img src="./img/Discover.png" alt="Discover" class="card-img">
+      </div>
+    </div>
+  </div>`;
   cardActve("Discover");
   const exam = document.querySelector("#Discover")
   expect(exam.classList.contains("mask")).toEqual(false);
